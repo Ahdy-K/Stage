219aeff41194ff835ac0e5a0d310a8ed0a202628
waeltoken <waelsmtv@gmail.com>
2022-10-08 12:56:47 +0100

    front angular

---
+++ stagi.tn/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SignupComponent } from './user/components/signup/signup.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+     
+    
+    
+    
+    
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
